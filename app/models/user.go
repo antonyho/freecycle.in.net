cbf04c1caa60f90a1bd1870b802ca1b26984527b
antonyho <ntonyworkshop@gmail.com>
2014-03-18 18:56:00 +0800

    Remove unnecessary attribute

--- app/models/user.go
+++ app/models/user.go
@@ -1,7 +1,6 @@
 package models
 
 type User struct {
-	Id       uint   // database generated
 	Email    string // verified email address for logon authentication and contact
 	Password string // should be a salted hash string
 }
