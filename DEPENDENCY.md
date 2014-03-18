5510252328152525aa60ab1ecb517595e84eb733
antonyho <ntonyworkshop@gmail.com>
2014-03-18 19:01:59 +0800

    Add MongoDB related stuffs

---
+++ DEPENDENCY.md
@@ -0,0 +1,15 @@
+Dependency
+===============
+
+- MongoDB [mongodb.org](mongodb.org)
+- mgo go lang driver [http://labix.org/mgo](http://labix.org/mgo)
+
+
+***
+
+### Installing mgo
+go get labix.org/v2/mgo
+
+
+***
+
---
+++ app/utils/dbutils.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"github.com/revel/revel"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
+
+type DbUtils struct {
+}
+
+func (u *DbUtils) GetSession() (*mgo.Session, *mgo.Database) {
+	// read database config
+	dsn, dsnFound := revel.Config.String("db.dsn")
+	if dsnFound {
+		session, err := mgo.Dial(dsn)
+		if err == nil {
+			db := session.DB("freecycle")
+
+			return session, db
+		} else {
+			revel.ERROR.Fatal("Cannot establish database connection")
+		}
+	} else {
+		revel.ERROR.Fatal("Cannot find DSN from config")
+	}
+
+	return nil, nil
+}
