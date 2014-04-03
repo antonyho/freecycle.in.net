dc607d0c134ac11015ac5bf8ec1c95690717f95e
antonyho <ntonyworkshop@gmail.com>
2014-04-03 14:25:26 +0800

    Interface update

--- app/controllers/post.go
+++ app/controllers/post.go
@@ -32,15 +32,15 @@ func (p *Post) List() revel.Result {
 	return p.Render()
 }
 
-func (p *Post) Update(models.Item) revel.Result {
-	return p.Render()
-}
-
 /*
 this function can be used by both for post owner and public
 this should be requested by AJAX and response with JSON
 public user should not be able to update post
 */
+func (p *Post) Update(models.Item) revel.Result {
+	return p.Render()
+}
+
 func (p *Post) ListFor(models.User) revel.Result {
 	return p.Render()
 }
