bfde11d561ff443b857df2fd8bc7d867809234b4
Antony Ho <ntonyworkshop@gmail.com>
2014-04-07 23:07:10 +0800

    Implementation on posting

--- app/controllers/post.go
+++ app/controllers/post.go
@@ -4,7 +4,6 @@ import (
 	"github.com/antonyho/freecycle.in.net/app/models"
 	"github.com/antonyho/freecycle.in.net/app/utils"
 	"github.com/revel/revel"
-	"log"
 	"time"
 )
 
@@ -18,23 +17,33 @@ func (p *Post) NewForm() revel.Result {
 
 func (p *Post) New(item models.Item) revel.Result {
 	now := time.Now().Unix()
-	log.Println(p.Params)
 	item.PostDate = now
 	item.UpdateDate = now
+	
+	// TODO add validations
+
 	dbutils := new(utils.DbUtils)
-	session, _ := dbutils.GetSession()
+	session, db := dbutils.GetSession()
 	defer session.Close()
-	itemCollection := session.DB("freecycle").C("item")
+	itemCollection := db.C("item")
 	err := itemCollection.Insert(item)
 	if err != nil {
 		revel.ERROR.Println("Cannot insert record in to 'item'")
 		revel.ERROR.Println(err)
 	}
 
+	// TODO Search for the tags in current post. Ff any tag is not created. insert it to database.
+
 	return p.Render()
 }
 
 func (p *Post) List() revel.Result {
+	dbutils := new(utils.DbUtils)
+	session, db := dbutils.GetSession()
+	defer session.Close()
+	itemCollection := db.C("item")
+	// TODO find all Action items ordered by date
+
 	return p.Render()
 }
 
@@ -43,14 +52,21 @@ this function can be used by both for post owner and public
 this should be requested by AJAX and response with JSON
 public user should not be able to update post
 */
-func (p *Post) Update(models.Item) revel.Result {
+func (p *Post) Update(item models.Item) revel.Result {
+	now := time.Now().Unix()
+	item.UpdateDate = now
+
 	return p.Render()
 }
 
-func (p *Post) ListFor(models.User) revel.Result {
+func (p *Post) ListFor(user models.User) revel.Result {
 	return p.Render()
 }
 
-func (p *Post) Request(models.Request) revel.Result {
+func (p *Post) ListBy(tag models.Tag) revel.Result {
+	return p.Render()
+}
+
+func (p *Post) Request(req models.Request) revel.Result {
 	return p.Render()
 }
