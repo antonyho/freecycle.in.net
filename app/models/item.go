9e59025bf9c81bc9c2885e60a8154de4225dbf12
Antony Ho <ntonyworkshop@gmail.com>
2014-04-14 04:32:36 +0800

    Implement information tooltip for listed item
    - Implement information tooltip box
    - Improve UI
    - Add new status for item post

--- app/models/item.go
+++ app/models/item.go
@@ -5,9 +5,9 @@ type Item struct {
 	Description   string
 	Tags          string // seperated by comma, should be mapped with Tag model in database
 	Condition     uint8  // percentage of status after depreciation
-	Status        string // ([A]vailable / [H]old / [N]ot available...will be hidden) only avabile when it is posted by registered user
+	Status        string // ([P]ending / [A]vailable / [H]old / [N]ot available...will be hidden) only avabile when it is posted by registered user
 	Duration      int    // list duration. default 7 days
-	Location      string // current location, free text, can be just district
+	Location      string // pickup location, free text, can be just district
 	Handover      string // ([F]ace / [D]elivery) handover method
 	Delivery      string // ([P]ost / [C]ourrier) delivery method if not handover
 	ContactMethod string // free text for user
