fc00e53cbedad6991e4ee4be75a3b52787880cfd
Antony Ho <ntonyworkshop@gmail.com>
2014-04-14 04:28:57 +0800

    Implement tag suggestion
    - Add dependency information about tag manager typeahead feature
    - Implement tag list function for tag suggestion feature

--- DEPENDENCY.md
+++ DEPENDENCY.md
@@ -4,6 +4,7 @@ Dependency
 - MongoDB [mongodb.org](mongodb.org)
 - mgo go lang driver [http://labix.org/mgo](http://labix.org/mgo)
 - Bootstrap Tag Manager [http://timschlechter.github.io/bootstrap-tagsinput/examples/](http://timschlechter.github.io/bootstrap-tagsinput/examples/)
+- The Typeahead plugin from Twitter's Bootstrap 2 to use with Bootstrap 3 [https://github.com/bassjobsen/Bootstrap-3-Typeahead](https://github.com/bassjobsen/Bootstrap-3-Typeahead)
 - reCAPTCHA [http://www.google.com/recaptcha](http://www.google.com/recaptcha)
 
 
@@ -20,3 +21,11 @@ go get labix.org/v2/mgo
 
 
 ***
+
+### Downloading typeahead.js
+[http://twitter.github.io/typeahead.js/](http://twitter.github.io/typeahead.js/)
+
+***
+
+### Downloading Bootstrap 3 Typeahead backport
+[https://github.com/bassjobsen/Bootstrap-3-Typeahead](https://github.com/bassjobsen/Bootstrap-3-Typeahead)
--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,12 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+	"github.com/antonyho/freecycle.in.net/app/utils"
+	"github.com/antonyho/freecycle.in.net/app/models"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+)
 
 type App struct {
 	*revel.Controller
@@ -12,4 +18,30 @@ func (c App) Index() revel.Result {
 
 func (c App) Objectives() revel.Result {
 	return c.Render()
+}
+
+func (c App) Tags(q string) revel.Result {
+	dbutils := new(utils.DbUtils)
+	session, db := dbutils.GetSession()
+	defer session.Close()
+	tagCollection := db.C("tag")
+	var tagList []models.Tag
+	var query *mgo.Query
+	if len(q) > 0 {
+		query = tagCollection.Find(bson.M{"name": bson.RegEx{q, "i"}}).Sort("name")
+	} else {
+		query = tagCollection.Find(nil).Sort("name")
+	}
+	
+	err := query.All(&tagList)
+
+	if err != nil {
+		revel.ERROR.Println(err)
+	}
+
+	if tagList == nil {
+		tagList = []models.Tag{}
+	}
+
+	return c.RenderJson(tagList)
 }
